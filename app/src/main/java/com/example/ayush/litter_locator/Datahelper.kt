d87f9b1fdd3b7e6488a6edc162ec339d2d5209bb
RiptideKing <2018kucp1009@iiitkota.ac.in>
2019-01-13 23:16:17 +0530

    First Commit

---
+++ app/src/main/java/com/example/ayush/litter_locator/Datahelper.kt
@@ -0,0 +1,26 @@
+package com.example.ayush.litter_locator
+
+class Datahelper {
+
+object car{
+    var testtry =false
+    var wasteHtype: String = "Common"
+    var wasteHinfo: String = "NOT PROVIDED"
+    var wasteHloc: String = "MNIT JAIPUR"
+    var wasteHuri: String ?= null
+    var wasteHname:String = "null"
+    var wasteHemail: String = "emailname"
+    var ttr:String = "ayush"
+    var wasteHh: String = "AYUSH"
+    var Hinfo:ArrayList<String> = arrayListOf()
+    var Hname:ArrayList<String> = arrayListOf()
+    var Htype:ArrayList<String> = arrayListOf()
+    var Huri:ArrayList<String> = arrayListOf()
+    var Hloc:ArrayList<String> = arrayListOf()
+    //var Hloc:ArrayList<String>?=null
+    var pnts = 0
+    var picuri:String?=null
+}
+
+
+}
